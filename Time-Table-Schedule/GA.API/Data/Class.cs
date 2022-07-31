92ab90bcc71994edc215a68842fccea0be5b4ea8
Mishrule <Mishrule1@gmail.com>
2022-07-31 18:14:34 +0000

    Testing Algorithm with data

---
+++ Time-Table-Schedule/GA.API/Data/Class.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GA.API.Data
+{
+    [Keyless]
+    public class Class
+    {
+        public int Professor { get; set; }
+        public int Course { get; set; }
+        public int Duration { get; set; }
+        public List<int> Groups { get; set; }
+        public bool Lab { get; set; }
+        public int GroupID { get; set; }
+        public virtual Group Group { get; set; }
+    }
+}
---
+++ Time-Table-Schedule/GaSchedule.Console/ConsoleApp.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using GaSchedule.Algorithm;
+
+namespace GaSchedule
+{
+    class ConsoleApp
+    {
+        static void Main(string[] args)
+        {       
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            var FILE_NAME = args.Length > 0 ? args[0] : "GaSchedule.json";
+            var configuration = new GaSchedule.Algorithm.Configuration();
+            configuration.ParseFile(FILE_NAME);
+
+            // var alg = new GeneticAlgorithm<Schedule>(new Schedule(configuration));
+            var alg = new Amga2<Schedule>(new Schedule(configuration));
+
+            System.Console.WriteLine("GA Schedule Version {0} C# .NET Core. Making a Class Schedule Using {1}.", Assembly.GetExecutingAssembly().GetName().Version, alg.ToString());
+            System.Console.WriteLine(".......");
+
+            alg.Run();
+            var htmlResult = HtmlOutput.GetResult(alg.Result);
+
+            var tempFilePath = Path.GetTempPath() + FILE_NAME.Replace(".json", ".htm");
+            using (StreamWriter outputFile = new StreamWriter(tempFilePath))
+            {
+                outputFile.WriteLine(htmlResult);
+            }
+            System.Console.WriteLine("");
+            System.Console.WriteLine(@"Completed in {0:s\.fff} secs with peak memory usage of {1}.", stopwatch.Elapsed, Process.GetCurrentProcess().PeakWorkingSet64.ToString("#,#"));
+
+            using (var proc = new Process())
+            {
+                proc.StartInfo.FileName = tempFilePath;
+                proc.StartInfo.UseShellExecute = true;
+                proc.StartInfo.Verb = "open";
+                proc.Start();
+            }
+        }
+    }
+}
